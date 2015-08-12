0b4411ce03ef434c30d84268130e7ad8a438fb08
MuhRizqiyawan <muh.rizqiyawan@gmail.com>
2015-08-12 13:14:55 +0700

    commit ke 3

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'about/index'
+  get 'about', to: 'about#index'
 
   resources :members
   # The priority is based upon order of creation: first created -> highest priority.
